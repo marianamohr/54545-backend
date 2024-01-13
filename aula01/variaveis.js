e1d8782a9f7eac4bcfca2d744b07a8bedc5acb59
Mari Mohr <marianamohr@gmail.com>
2024-01-13 11:10:35 -0300

    refatoração aula 1

--- aula01/variaveis.js
+++ aula01/variaveis.js
@@ -8,20 +8,21 @@ const objeto = {
   professor: true,
   tutor: false,
 };
-
-const array = [1, 2, 3, 4, 5];
+//     index    0   1   2   3   4
+const array = [ 1 , 2 , 3 , 4 , 5];
+               
 
 const educadores = [
   {
     nome: "Mari",
     idade: 34,
-    professor: true,
+    professor: true,  // index 0
     tutor: false,
   },
   {
     nome: "Guilherme",
     idade: 34,
-    professor: false,
+    professor: false, // index 1
     tutor: true,
   },
 ];
